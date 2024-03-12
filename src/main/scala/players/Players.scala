a7b3eded035f0e2f4bb95679f1cb6597935eeb9d
jojogilx <jofilipemorais@gmail.com>
2024-03-12 08:46:06 +0000

    removed Player object

--- src/main/scala/players/Players.scala
+++ src/main/scala/players/Players.scala
@@ -4,54 +4,53 @@ import card._
 import cats.implicits.catsSyntaxOptionId
 
 
-object Players {
+type PlayerID = String
+private type Hand = List[Card]
 
-  type PlayerID = String
-  private type Hand = List[Card]
+case class Player(playerID: PlayerID, textColor: String) {
+  private var cards = List.empty[Card] //todo: remove var
 
-  case class Player(playerID: PlayerID, textColor: String) {
-    private var cards = List.empty[Card]
-
-    def hand: Hand = {
-      val ret = cards
-      ret
-    }
-
-    def tryGetDefuse: Option[Card] =
-      cards.zipWithIndex.find { case (card, _) => card match {
-        case Defuse => true
-        case _ => false
-      }} match {
-        case Some((card, i)) =>
-          val (left, right) = cards.splitAt(i)
-          cards = left ::: right.drop(1)
-          card.some
-        case None => None
-      }
-
-    def playCard(index: Int): Card = {
-      val card = cards(index)
-      val (left, right) = cards.splitAt(index)
-      cards = left ::: right.drop(1)
-      card
-    }
-
-    def initHand(hand: Hand): Unit = {
-      cards = hand
-    }
-
-    def drawCard(card: Card): Unit = {
-      cards = cards :+ card
-    }
-
-
-    def handWithIndex(): String = {
-      cards.zipWithIndex.map {
-        case (card, i) => s"${i + 1}. $card   "
-      }.mkString
-    }
-
-
+  def hand: Hand = {
+    val ret = cards
+    ret
   }
 
+  def tryGetDefuse: Option[Card] =
+    cards.zipWithIndex.find { case (card, _) => card match {
+      case Defuse => true
+      case _ => false
+    }} match {
+      case Some((card, i)) =>
+        val (left, right) = cards.splitAt(i)
+        cards = left ::: right.drop(1)
+        card.some
+      case None => None
+    }
+
+  def playCard(index: Int): Card = {
+    val card = cards(index)
+    val (left, right) = cards.splitAt(index)
+    cards = left ::: right.drop(1)
+    card
+  }
+
+  def initHand(hand: Hand): Unit = {
+    cards = hand
+  }
+
+  def drawCard(card: Card): Unit = {
+    cards = cards :+ card
+  }
+
+
+  def handWithIndex(): String = {
+    cards.zipWithIndex.map {
+      case (card, i) => s"${i + 1}. $card   "
+    }.mkString
+  }
+
+}
+
+object Player {
+  //see if smt should be here
 }
